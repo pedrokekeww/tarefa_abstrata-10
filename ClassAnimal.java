2de66a53cedb0e98bb76bbf3c01d6666062d3f1a
pedrokekww <pedroveb119@gmail.com>
2025-02-07 14:17:11 -0300

    Create ClassAnimal.java

---
+++ ClassAnimal.java
@@ -0,0 +1,11 @@
+class Animal {
+    protected String nome;
+
+    public Animal(String nome) {
+        this.nome = nome;
+    }
+
+    public String emitirSom() {
+        return "Som genérico de animal";
+    }
+}
